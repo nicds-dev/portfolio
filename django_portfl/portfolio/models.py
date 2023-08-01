65dd9d8ac90df939cc75f0ed57edcdfe9ed33eeb
NicDs <nikolasdurango@gmail.com>
2023-07-31 20:45:20 -0500

    app portfolio, models v1.0

--- django_portfl/portfolio/models.py
+++ django_portfl/portfolio/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField('Project Title', max_length=150)
+    description = models.CharField('Project Description', max_length=254)
+    image = models.ImageField(
+        'Project Image',
+        upload_to=None,
+    )
+    url = models.URLField('Project Url', blank=True)
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        return self.title
